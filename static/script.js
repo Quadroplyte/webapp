fd70c92931cdbd700cf9272caf046aa27b085825
nol0n <sharapovegor2003@gmail.com>
2026-02-24 16:43:26 +0300

    wide\short behaviour changed

--- static/script.js
+++ static/script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Automatic Behavior on Resize
   const handleResize = () => {
-    if (window.innerWidth < 1024) {
+    if (window.innerWidth < 1201) {
       sidebar.classList.add('collapsed');
     } else {
       // On larger screens, remove the mobile-open state if browser was resized
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!clickedItem) return;
 
     // On mobile, close menu after clicking a link
-    if (window.innerWidth < 1024) {
+    if (window.innerWidth < 1201) {
       closeMobileMenu();
     }
 
